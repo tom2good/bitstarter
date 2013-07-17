57baae3f7fb611fbcfdf0c1e8c7c6c84370a9859
Tom 2 Good <tom2good@yahoo.com>
2013-07-17 06:10:58 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+// file name
+var inputFile = "./index.html";
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    response.send(fs.readFileSync(inputFile, 'utf-8'));
+//  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
